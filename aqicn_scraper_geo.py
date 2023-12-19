bc60eaaaf3c9d208f5db92d148c6c165b2eaab1d
Ivanf0e <152105200+Ivanf0e@users.noreply.github.com>
2023-12-19 12:21:43 -0800

    Add files via upload

--- aqicn_scraper_geo.py
+++ aqicn_scraper_geo.py
@@ -1,8 +1,11 @@
 import requests
 import concurrent.futures
 from time import time
-import numpy as np
+import json
 
+with open ('waqi_stations.json', 'r') as f:
+    y = json.load(f)
+    
 def request_get(url):
     return requests.get(url=url)
 
@@ -17,13 +20,24 @@ def scraper(stations,token):
 
 if __name__ == "__main__":
 #     geo = [37.36927,-122.01275] # current position #todo1 update position from location service
-    stations = range(184858,184898) #246229) # [1213,184858, 224008, 201238,246229,13759] #,'A224485','A153037'] #todo1 find closest stations st 0<w<1
     tkn = open('waqi_token.txt').read()
+    s, ds, np = 229900, 100, 0
+    d = {}
     t0 = time()
-    r = scraper(stations, tkn)
+    while s < 240000:
+#         stations = : #184858,184878) #246229) # [1213,184858, 224008, 201238,246229,13759] #,'A224485','A153037'] #todo1 find closest stations st 0<w<1
+        r = scraper(range(s,s+ds), tkn) 
+        for v in r:
+            if v['status'] == 'ok':
+                d[-v['data']['idx']] = {'geo': v['data']['city']['geo'], 'qntt': list(v['data']['iaqi'].keys())} 
+        s += ds; np += ds
+        print('polled/found stations: %.0f, %.0f (%.1f%%)' % (np, ns := len(d), ns/np*100))
+    dd = {str(k):v for k,v in d.items() if str(k) not in y.keys()}
     print('total time = %.1f s' % (dt:=time() -t0))
-    d = {-v['data']['idx']: v['data']['city']['geo'] for v in r if v['status'] == 'ok'}
-    print('polled/found stations: %.0f, %.0f (%.1f%%)' % (np := len(stations), ns := len(d), ns/np*100))
     print('API polls per second (max = 1000): %.0f' % (np / dt))
-    
+    print('new/old stations found: %.0f, %.0f' % (
+        len(dd), sum([str(k) in y.keys() for k in d.keys()])))
+#     y.update(dd)
+#     with open ('waqi_stations.json', 'w') as f:
+#         json.dump(y, f)
 #     [print(k,v) for k,v in d.items()]
